fd1c11eb0a2c241084e50154936e0ab1a128425f
Adam Short <adamshort1994@gmail.com>
2016-05-06 19:55:20 +0100

    use getmapasync instead of deprecated getmap

--- app/src/main/java/com/adamshort/canieatthis/PlacesFragment.java
+++ app/src/main/java/com/adamshort/canieatthis/PlacesFragment.java
@@ -19,12 +19,13 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PlacesFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks {
+public class PlacesFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, OnMapReadyCallback {
 
     private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 1;
 
@@ -37,15 +38,14 @@ public class PlacesFragment extends Fragment implements GoogleApiClient.Connecti
     private GoogleMap googleMap;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // inflat and return the layout
         View v = inflater.inflate(R.layout.fragment_places, container,
                 false);
         mMapView = (MapView) v.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
-
         mMapView.onResume();// needed to get the map to display immediately
+        mMapView.getMapAsync(this);
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
@@ -59,10 +59,22 @@ public class PlacesFragment extends Fragment implements GoogleApiClient.Connecti
                 .build();
         mGoogleApiClient.connect();
 
-        googleMap = mMapView.getMap();
+        // Perform any camera updates here
+        return v;
+    }
+
+    private void moveCamera(GoogleMap googleMap, double latitude, double longitude) {
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
         // latitude and longitude
-        latitude = 17.385044;
-        longitude = 78.486671;
+        latitude = 51.5572700;
+        longitude = -0.2838810;
 
         // create marker
         MarkerOptions marker = new MarkerOptions().position(
@@ -84,53 +96,51 @@ public class PlacesFragment extends Fragment implements GoogleApiClient.Connecti
             Log.d("DEBUG", "Didn't have needed permission, requesting ACCESS_FINE_LOCATION");
             ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
         }
-
-        // Perform any camera updates here
-        return v;
-    }
-
-    private void moveCamera(GoogleMap googleMap, double latitude, double longitude) {
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(latitude, longitude)).zoom(12).build();
-        googleMap.animateCamera(CameraUpdateFactory
-                .newCameraPosition(cameraPosition));
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mMapView.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mMapView.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mMapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         Log.d("DEBUG", "onConnected");
-        if (ContextCompat.checkSelfPermission(getActivity().getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            latitude = mLastLocation.getLatitude();
-            longitude = mLastLocation.getLongitude();
-            moveCamera(googleMap, latitude, longitude);
-        } else {
-            Log.d("DEBUG", "Didn't have needed permission, requesting ACCESS_FINE_LOCATION");
-            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
+//        if (ContextCompat.checkSelfPermission(getActivity().getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//            latitude = mLastLocation.getLatitude();
+//            longitude = mLastLocation.getLongitude();
+//            moveCamera(googleMap, latitude, longitude);
+//        } else {
+//            Log.d("DEBUG", "Didn't have needed permission, requesting ACCESS_FINE_LOCATION");
+//            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
+//        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mMapView != null) {
+            mMapView.onPause();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mMapView != null) {
+            mMapView.onDestroy();
+        }
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if (mMapView != null) {
+            mMapView.onLowMemory();
         }
     }
 
