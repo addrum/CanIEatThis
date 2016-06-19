7ab59b37f9495fdd0a17320781954a431f3e66ce
Adam Short <adamshort1994@gmail.com>
2016-06-19 21:33:27 +0100

    implement intro

--- app/src/main/java/com/adamshort/canieatthis/AppIntroActivity.java
+++ app/src/main/java/com/adamshort/canieatthis/AppIntroActivity.java
@@ -2,6 +2,7 @@ package com.adamshort.canieatthis;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
@@ -13,12 +14,37 @@ public class AppIntroActivity extends IntroActivity {
         super.onCreate(savedInstanceState);
 
         addSlide(new SimpleSlide.Builder()
-                .title("first title")
-                .description("first desc")
+                .title("CanIEatThis")
+                .description("Welcome!")
                 .image(R.mipmap.ic_launcher)
                 .background(R.color.colorPrimary)
                 .backgroundDark(R.color.colorPrimaryDark)
-                .permission(Manifest.permission.INTERNET)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Download Offline Database")
+                .description("To enable offline scanning of products, we need to download the database. " +
+                        "Press the button below to do that now! (You will see a download notification appear.")
+                .buttonCtaLabel("Download Database")
+                .buttonCtaClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Utilities.downloadDatabase(AppIntroActivity.this, getBaseContext());
+                    }
+                })
+                .image(R.mipmap.ic_launcher)
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Finding Restaurants Near You")
+                .description("In order to find restaurants, we need access to your location. " +
+                        "Press the button below to do that now!")
+                .permission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .image(R.mipmap.ic_launcher)
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
                 .build());
     }
 }
