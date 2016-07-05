6ada32d4a77c80da8e5bfef1a12de8334bfd2bf2
Adam Short <adamshort1994@gmail.com>
2016-07-05 13:19:06 +0100

    remove debug so we can scan and reenable minify

--- app/src/main/java/com/adamshort/canieatthis/ui/fragment/ScanFragment.java
+++ app/src/main/java/com/adamshort/canieatthis/ui/fragment/ScanFragment.java
@@ -178,7 +178,7 @@ public class ScanFragment extends Fragment {
             //start the scanning activity from the com.google.zxing.client.android.SCAN intent
 //            Intent intent = new Intent(ACTION_SCAN);
 //            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
-            if (DEBUG) {
+//            if (DEBUG) {
 //                getBarcodeInformation("7622210307668");
             // McVities Digestives
 //                getBarcodeInformation("5000168001142");
@@ -196,10 +196,10 @@ public class ScanFragment extends Fragment {
 //                getBarcodeInformation("5053990101863");
 //                Intent intentDebug = new Intent(getContext(), AddProductActivity.class);
 //                startActivityForResult(intentDebug, FORM_REQUEST_CODE);
-            } else {
+//            } else {
 //                startActivityForResult(intent, 0);
             IntentIntegrator.forSupportFragment(this).initiateScan();
-            }
+//            }
         } catch (ActivityNotFoundException anfe) {
             //on catch, show the download dialog
             showDialog(this.getActivity(), "No Scanner Found", "Download a scanner now?", "Yes", "No", DOWNLOAD).show();
