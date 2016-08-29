53f060bcec4f8bc8c0a3b6027cd9546fa59ae983
Adam Short <adamshort1994@gmail.com>
2016-08-29 20:38:33 +0100

    remove unnecessary add info from wear activity as it's now handled on watch

--- app/src/main/java/com/adamshort/canieatthis/app/util/ListenerService.java
+++ app/src/main/java/com/adamshort/canieatthis/app/util/ListenerService.java
@@ -6,7 +6,6 @@ import android.util.Log;
 
 import com.adamshort.canieatthis.R;
 import com.adamshort.canieatthis.app.data.PlacesHelper;
-import com.adamshort.canieatthis.app.ui.activity.AddInfoFromWearActivity;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
@@ -25,13 +24,6 @@ public class ListenerService extends WearableListenerService implements NextPage
         Log.d("onMessageReceived", "Message path received on phone is: " + messageEvent.getPath());
 
         switch (path) {
-            case "/add_places_info_phone_path":
-                // Broadcast message to wearable activity for display
-                Intent intent = new Intent(this, AddInfoFromWearActivity.class);
-                intent.putExtra("message_from_watch", message);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                break;
             case "/request_markers":
                 doPlacesAPIRequest(message, null);
                 break;
