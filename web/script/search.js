0b5e0638d0205ad19d475d9240eb88a940e05a5b
Adam Short <adamshort1994@gmail.com>
2016-12-08 22:56:58 +0000

    add super basic working barcode search

---
+++ web/script/search.js
@@ -0,0 +1,23 @@
+(function() {
+	angular.module('CanIEatThisApp', [])
+	.controller('SearchController', function($scope, $http) {
+		$scope.$watch('search', function() {
+			fetch();
+		});
+
+		$scope.search = 5051140367282;
+
+		function fetch() {
+			$http.get("http://world.openfoodfacts.org/api/v0/product/" + $scope.search)
+			.then(function(response) {
+				$scope.name = response.data.product.product_name == '' 
+					? 'No product name found' : response.data.product.product_name
+				$scope.ingredients = response.data.product.ingredients_text == ''
+					? 'No ingredients found' : response.data.product.ingredients_text
+				$scope.traces = response.data.product.traces == ''
+					? 'No traces found' : response.data.product.traces
+				$scope.results = response.data;
+			});
+		}
+	});
+})();
